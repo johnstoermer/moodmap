95949a3e7a3c5e76e2b4d9ab7e283b81dba1d9a7
johnstoermer <john.stoermer@gmail.com>
2018-06-11 22:29:43 -0400

    changed the tab name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+      document.title = "moodmap"
     return (
       <div>
           <div>
-            <header>
-              <h1 className="App-title">moodmap</h1>
-            </header>
-            <p className="App-intro">
-                Web Application in React that maps the moods of various locations around the world based off of news for that location.
-
-                Made By: John Stoermer and Yatharth Rawat
-            </p>
+              <h1>moodmap</h1>
+              <p>Web Application in React that maps the moods of various locations around the world based off of news for that location.</p>
+              <p>Made By: John Stoermer and Yatharth Rawat</p>
           </div>
       </div>
     );
