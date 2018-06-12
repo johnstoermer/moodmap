10060cabe4dad0c8ce76e5e23e6a44a6ab8b62b3
johnstoermer <john.stoermer@gmail.com>
2018-06-11 22:18:18 -0400

    Edited some html in App

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <div>
+            <header>
+              <h1 className="App-title">moodmap</h1>
+            </header>
+            <p className="App-intro">
+                Web Application in React that maps the moods of various locations around the world based off of news for that location.
+
+                Made By: John Stoermer and Yatharth Rawat
+            </p>
+          </div>
       </div>
     );
   }
